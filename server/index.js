1ea01fb42e32729b47b166d8d83f475bd677dd79
Kcs111 <kcs111@txstate.edu>
2021-12-20 14:49:27 -0600

    server up

--- server/index.js
+++ server/index.js
@@ -4,3 +4,13 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://Kyla2021:Loveless54@cluster0.id7xo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL).then(()=>{console.log("Server up and running on port 5000")});
+
